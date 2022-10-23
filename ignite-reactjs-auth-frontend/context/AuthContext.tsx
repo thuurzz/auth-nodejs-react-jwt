b5f156c7bca7854109574046e538a43baf989511
Arthur Vinicius <thuur.vss@gmail.com>
2022-10-22 21:50:43 -0300

    Configurando cliente do axios

--- ignite-reactjs-auth-frontend/context/AuthContext.tsx
+++ ignite-reactjs-auth-frontend/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SingInCredentials = {
   email: string;
@@ -20,7 +21,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function singIn({ email, password }: SingInCredentials) {
-    console.log({ email, password });
+    try {
+      const resp = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
