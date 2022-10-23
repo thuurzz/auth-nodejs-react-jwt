2a97c527b34cb69636b4ecb10e97b801382985a2
Arthur Vinicius <thuur.vss@gmail.com>
2022-10-22 22:40:45 -0300

    Salvando dados do usuário

--- ignite-reactjs-auth-frontend/context/AuthContext.tsx
+++ ignite-reactjs-auth-frontend/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode } from "react";
+import Router from "next/router";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
 
 type SingInCredentials = {
@@ -6,9 +7,16 @@ type SingInCredentials = {
   password: string;
 };
 
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
 type AuthContextData = {
   singIn(credentials: SingInCredentials): Promise<void>;
   isAuthenticated: boolean;
+  user: User | undefined;
 };
 
 type AuthProviderProps = {
@@ -18,23 +26,37 @@ type AuthProviderProps = {
 const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  // usuario so estara presente, caso autenticacao true
+  const isAuthenticated = !!user;
 
   async function singIn({ email, password }: SingInCredentials) {
     try {
+      // realiza chamada para a api que autentica no backend
       const resp = await api.post("sessions", {
         email,
         password,
       });
 
-      console.log(resp.data);
+      // extrai lista de permissions e roles
+      const { token, refreshToken, permissions, roles } = resp.data;
+
+      // caso tudo retorne, cria usuario
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      // redireciona usuario para a rota
+      Router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ singIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ singIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
